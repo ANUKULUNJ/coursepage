80685802ffdf4ddc20c54a4f910b9ca01cb7d7a4
AdiNarang01 <narang.adi01@gmail.com>
2025-05-13 21:19:51 +0530

    Payment-changes-try

--- frontend/src/pages/AllCourses.jsx
+++ frontend/src/pages/AllCourses.jsx
@@ -99,7 +99,7 @@ const AllCoursesPage = () => {
       localStorage.setItem('registrationId', registrationResult.registration.registrationId);
       
       // Open Razorpay in a new tab
-      window.location.href('https://rzp.io/rzp/bDNGTXB', '_blank');
+      window.location.href = 'https://rzp.io/rzp/bDNGTXB';
       
       // Don't close the modal
       if (setIsSubmitted) {
